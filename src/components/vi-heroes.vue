95d838e824e44c8b32808482f142a5c3e2d6fb47
n101661 <n101661@gmail.com>
2023-08-25 19:35:31 +0800

    add ViHeroes component

---
+++ src/components/vi-heroes.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-select v-model="value">
+    <el-option
+      v-for="item in heroes"
+      :key="item"
+      :label="item"
+      :value="item"
+    />
+  </el-select>
+</template>
+
+<script lang="ts" setup>
+import { ElSelect, ElOption } from "element-plus"
+import { vindictusHeroes } from "./heroes.ts"
+import { ref } from "vue"
+
+const value = ref("")
+
+const heroes = vindictusHeroes.keys()
+</script>

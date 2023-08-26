51b88770dbf4e933c435dd4df0d086d527fe622b
n101661 <n101661@gmail.com>
2023-08-26 23:45:13 +0800

    add a component to define heroes you have

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>Hello World</div>
+  <h2>The Heroes You Have</h2>
+  <vi-select-heroes
+    v-model="hero"
+    @save="saveHeroes"
+  />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+import ViSelectHeroes from "./components/hero/vi-select.vue"
+
+const hero = ref<string[]>(getHeroes())
+
+function saveHeroes(value: string | string[] | undefined) {
+  if (!window.localStorage) {
+    return
+  }
+  window.localStorage.setItem("heroes", JSON.stringify(value))
+}
+
+function getHeroes(): string[] {
+  if (!window.localStorage) {
+    return []
+  }
+  const data = window.localStorage.getItem("heroes")
+  if (data == null) {
+    return []
+  }
+  return JSON.parse(data)
+}
+</script>
 
 <style scoped></style>
--- src/components/hero/vi-select.vue
+++ src/components/hero/vi-select.vue
@@ -2,6 +2,7 @@
   <el-select
     v-model="value"
     filterable
+    multiple
   >
     <el-option
       v-for="item in heroes"
@@ -13,11 +14,30 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue"
 import { ElSelect, ElOption } from "element-plus"
 import { vindictusHeroes } from "./heroes.ts"
-import { ref } from "vue"
-
-const value = ref("")
 
 const heroes = vindictusHeroes.keys()
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Array<string>],
+    default: undefined,
+  },
+})
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | string[] | undefined): void
+  (e: "save", value: string | string[] | undefined): void
+}>()
+
+const value = computed({
+  get(): string | string[] | undefined {
+    return props.modelValue
+  },
+  set(value: string | string[] | undefined) {
+    emit("update:modelValue", value)
+    emit("save", value)
+  },
+})
 </script>

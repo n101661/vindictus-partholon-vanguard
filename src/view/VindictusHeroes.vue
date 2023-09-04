0f5bac985d2b825a7f51ac78b1bb352d5be7f467
n101661 <n101661@gmail.com>
2023-09-04 20:48:36 +0800

    refactor VindictusHeroes component

--- src/view/VindictusHeroes.vue
+++ src/view/VindictusHeroes.vue
@@ -1,8 +1,122 @@
 <template>
   <h2>Heroes</h2>
-  <vi-form-heroes />
+  <hero-dialog
+    v-model="dialogVisible"
+    title="Create Hero"
+    @submit="addHero"
+  />
+
+  <el-table :data="heroes">
+    <el-table-column width="70px">
+      <template #header>
+        <el-button @click="dialogVisible = true">
+          <el-icon><Plus /></el-icon>
+        </el-button>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="Icon"
+      width="80px"
+    >
+      <template #default="{ $index }">
+        <el-image
+          v-if="$index <= officialHeroIndex"
+          style="width: 40px; height: 40px"
+          :src="heroImageURL($index + 1)"
+        />
+        <el-icon
+          v-else
+          style="width: 40px; height: 40px"
+        >
+          <Avatar />
+        </el-icon>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="Name"
+      width="100px"
+    >
+      <template #default="{ row }">
+        {{ getName((row as Hero).name) }}
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="specialties"
+      label="Specialties"
+    >
+      <template #default="{ row }">
+        <el-tag
+          v-for="(sp, i) in (row as Hero).specialties"
+          v-show="sp"
+          :key="i"
+        >
+          {{ sp }}
+        </el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup lang="ts">
-import ViFormHeroes from "../components/hero/ViForm.vue"
+import { ref, onBeforeMount } from "vue"
+import {
+  ElButton,
+  ElTable,
+  ElTableColumn,
+  ElImage,
+  ElIcon,
+  ElTag,
+  ElMessage,
+} from "element-plus"
+import { Plus, Avatar } from "@element-plus/icons-vue"
+import { Hero, vindictusHeroes, getName } from "../components/hero/heroes.ts"
+import HeroDialog from "../components/hero/HeroDialog.vue"
+
+const heroes = Array.from(
+  vindictusHeroes.entries(),
+  (v: [string, string[]]): Hero => {
+    return new Hero(v[0], [...v[1]])
+  },
+)
+const officialHeroIndex = heroes.length - 1
+
+const dialogVisible = ref(false)
+
+onBeforeMount(() => {
+  if (!window.localStorage) return
+  const extraHeroes: Hero[] = JSON.parse(
+    window.localStorage.getItem("extra-heroes") ?? "[]",
+  )
+  for (const hero of extraHeroes) {
+    heroes.push(hero)
+  }
+})
+
+function addHero(v: Hero) {
+  if (!window.localStorage) return
+
+  const extraHeroes: Hero[] = JSON.parse(
+    window.localStorage.getItem("extra-heroes") ?? "[]",
+  )
+  const hero: Hero = {
+    name: v.name,
+    specialties: [...v.specialties],
+  }
+  extraHeroes.push(hero)
+  window.localStorage.setItem("extra-heroes", JSON.stringify(extraHeroes))
+
+  heroes.push(hero)
+
+  ElMessage({
+    message: `Created ${v.name} successful`,
+    type: "success",
+  })
+}
+
+function heroImageURL(i: number): string {
+  return (
+    "https://twskyimg.akamaized.net/gameweb/mh/home/img/heros/" + i + "@2x.png"
+  )
+}
 </script>
